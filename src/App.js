fbc9f09ba6309bd42a1835dd91275e614206f504
laska-adief <laskaadief.la@gmail.com>
2023-04-14 11:06:17 +0700

    Quote Generator React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [error, setError] = useState("");
+
+  const fetchQuote = () => {
+    axios
+      .get("	https://api.adviceslip.com/advice")
+      .then((res) => {
+        const { advice } = res.data.slip;
+        setAdvice(advice);
+      })
+      .catch((err) => {
+        console.log("err", err);
+        setError("There was an error, please refresh the page and try to generate again...");
+      });
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <h1 className="heading">Quote Generator</h1>
+      <div className="card">
+        <p className="quote">{error ? error : advice}</p>
+        <button className="button" onClick={fetchQuote}>
+          Generate Quote
+        </button>
+      </div>
+      <div className="footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created by :{" "}
+          <a href="https://github.com/laska-adief" target="_blank" rel="noreferrer">
+            Laska Adief
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
-
 export default App;
